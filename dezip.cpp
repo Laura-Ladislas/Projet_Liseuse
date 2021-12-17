91f815fa764a0947cca7f2c5684e3d674129cead
Laura LADISLAS <laura.ladislas@ensta-paris.fr>
2021-12-17 12:17:47 +0100

    DEZIP + dossier

--- dezip.cpp
+++ dezip.cpp
@@ -3,6 +3,7 @@
 #include <wx/wfstream.h>
 #include <iostream>
 #include <memory>
+#include <vector>
 
 int main()
 {
@@ -10,23 +11,31 @@ int main()
 	wxFFileInputStream in(wxT("test.cbz"));
 	wxZipInputStream zip(in);
 
+	//Parcourt tous les élements de l'archive et les extrait tend qu'elle est pas vide
 	while (entry.reset(zip.GetNextEntry()), entry.get() != NULL)
 	{
 	    // access meta-data
 	    wxString name = entry->GetName();
-	    wxFileName new_file;
+	    wxString strTargetDir = "../image";
+	    wxString strFileName = strTargetDir + wxFileName::GetPathSeparator() + name;
+
+	    // Ne sert pas à dezipper, sert à gérer si c'est un fichier ou un dossier
+	    /*wxFileName new_file;
 	    // read 'zip' to access the entry's data
 	    if (entry->IsDir())
 	    {
-	    	new_file.AssignDir(name);
+	    	//Ajouter des affichages pour comprendre ce qu'il se passe et si le strFileName est bien créer
+	    	new_file.AssignDir(strFileName);
 	    }
 	    else
 	    {
-	        new_file.Assign(name);
-	    }
+	        new_file.Assign(strFileName);
+	    }*/
 
-	    wxFileOutputStream file(name);
+	    //strFileName doit contenir le path du dossier dans lequel on veut read (donc extraire) nos fichiers
+	    wxFileOutputStream file(strFileName);
 	    zip.Read(file);
 	}
 	return 0;
 }
+
